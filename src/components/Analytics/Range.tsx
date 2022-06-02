7b56c4aecb5063b6583f1f6d4399d4fb2a4f2563
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-06-01 21:56:25 -0300

    feat(topic): add chart to analytics

---
+++ src/components/Analytics/Range.tsx
@@ -0,0 +1,113 @@
+import { Box, Heading, useToken, StyleProps } from "@chakra-ui/react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ChartOptions,
+} from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement);
+
+interface DatasetSizes {
+  barPercentage?: number;
+  categoryPercentage?: number;
+}
+
+export interface RangeProps extends StyleProps {
+  title: string;
+  labels: string[];
+  data: number[];
+  scaleX?: string;
+  scaleY?: string;
+  datasetSizes?: DatasetSizes;
+}
+
+export const Range = ({
+  title,
+  scaleX,
+  scaleY,
+  labels,
+  data,
+  datasetSizes = {
+    barPercentage: 1,
+    categoryPercentage: 0.3,
+  },
+  ...rest
+}: RangeProps) => {
+  const [brand300, brand500] = useToken("colors", ["brand.300", "brand.500"]);
+
+  const sortedData = [...data].sort((a, b) => a - b);
+  const biggestNumber = sortedData[sortedData.length - 1];
+  const barColors = data.map((number) =>
+    number === biggestNumber ? brand300 : brand500
+  );
+
+  const options: ChartOptions<"bar"> = {
+    indexAxis: "y",
+    responsive: true,
+    maintainAspectRatio: false,
+    color: "#fff",
+    scales: {
+      y: {
+        title: {
+          display: true,
+          align: "start",
+          text: scaleY,
+          color: "#fff",
+          font: {
+            size: 12,
+          },
+        },
+        ticks: {
+          color: "#fff",
+          font: {
+            size: 12,
+          },
+        },
+      },
+      x: {
+        title: {
+          display: true,
+          align: "start",
+          text: scaleX,
+          color: "#fff",
+          font: {
+            size: 12,
+          },
+        },
+        ticks: {
+          color: "#fff",
+          font: {
+            size: 12,
+          },
+        },
+      },
+    },
+  };
+
+  const chartData = {
+    labels,
+    datasets: [
+      {
+        ...datasetSizes,
+        backgroundColor: barColors,
+        data,
+      },
+    ],
+  };
+
+  return (
+    <Box bgColor="#525F7F4D" p="40px" borderRadius="10px" {...rest}>
+      <Box borderBottom="1px solid #525F7F" pb={2}>
+        <Heading fontSize="18px" fontWeight={500} color="white">
+          {title}
+        </Heading>
+      </Box>
+      <Box maxHeight={`${60 * labels.length}px`}>
+        <Bar options={options} data={chartData} />;
+      </Box>
+    </Box>
+  );
+};
--- src/pages/topic/analytics.tsx
+++ src/pages/topic/analytics.tsx
@@ -1,5 +1,34 @@
-import { Heading } from "@chakra-ui/react";
+import { Range } from "../../components/Analytics";
 
 export const Analytics = () => {
-  return <Heading color="white">Analytics</Heading>;
+  const confidentLabels = ["5", "4", "3", "2", "1"];
+  const confidentData = confidentLabels.map(
+    () => Math.floor(Math.random() * 10) + 1
+  );
+
+  const blockLabels = ["Y", "N"];
+  const BlockData = blockLabels.map(() => Math.floor(Math.random() * 10) + 1);
+
+  return (
+    <>
+      <Range
+        title="How confident are you?"
+        scaleX="People"
+        scaleY="Satisfaction"
+        labels={confidentLabels}
+        data={confidentData}
+      />
+
+      <Range
+        title="Is something blocking you?"
+        scaleX="People"
+        labels={blockLabels}
+        data={BlockData}
+        datasetSizes={{
+          categoryPercentage: 0.5,
+        }}
+        mt={7}
+      />
+    </>
+  );
 };
